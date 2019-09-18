6b42612426f55e5a9b7cfdc35557c2d9155fed36
David Grudl <david@grudl.com>
2019-09-18 20:23:10 +0200

    test: removed dead code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,23 +18,6 @@ Tester\Environment::setup();
 date_default_timezone_set('Europe/Prague');
 
 
-// create temporary directory
-(function () {
-	define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
-
-	// garbage collector
-	$GLOBALS['\\lock'] = $lock = fopen(__DIR__ . '/lock', 'w');
-	if (rand(0, 100)) {
-		flock($lock, LOCK_SH);
-		@mkdir(dirname(TEMP_DIR));
-	} elseif (flock($lock, LOCK_EX)) {
-		Tester\Helpers::purge(dirname(TEMP_DIR));
-	}
-
-	@mkdir(TEMP_DIR);
-})();
-
-
 function test(\Closure $function): void
 {
 	$function();
