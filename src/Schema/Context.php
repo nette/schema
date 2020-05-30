25c91626401e08d20d7eba1b791f2e44b25e234f
David Grudl <david@grudl.com>
2020-05-30 14:48:40 +0200

    addError(): renamed 'hint' to 'expected'

--- src/Schema/Context.php
+++ src/Schema/Context.php
@@ -32,8 +32,8 @@ final class Context
 	public $dynamics = [];
 
 
-	public function addError($message, $hint = null)
+	public function addError($message, $expected = null)
 	{
-		$this->errors[] = (object) ['message' => $message, 'path' => $this->path, 'hint' => $hint];
+		$this->errors[] = (object) ['message' => $message, 'path' => $this->path, 'expected' => $expected];
 	}
 }
