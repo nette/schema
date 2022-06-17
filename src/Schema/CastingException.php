351edc711de975b2afb5c72a208dc92b8377a3e7
Daniel Vítek <kontakt@danielvitek.me>
2022-06-17 11:55:29 +0200

    Schema: added possibility to instantiate object using reflection Using reflection it is possible to instantiate object without constructor, which is pretty useful for constructor property promotion

---
+++ src/Schema/CastingException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (https://nette.org)
+ * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
+ */
+
+declare(strict_types=1);
+
+namespace Nette\Schema;
+
+use Nette;
+
+
+/**
+ * Casting error.
+ */
+class CastingException extends Nette\InvalidStateException
+{
+}
