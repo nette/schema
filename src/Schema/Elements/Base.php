003c17d3185925841fb6c0a0601178a426014b11
David Grudl <david@grudl.com>
2024-11-23 17:53:33 +0100

    used #Deprecated

--- src/Schema/Elements/Base.php
+++ src/Schema/Elements/Base.php
@@ -174,7 +174,7 @@ trait Base
 	}
 
 
-	/** @deprecated use Nette\Schema\Helpers::validateType() */
+	#[\Deprecated('use Nette\Schema\Helpers::validateType()')]
 	private function doValidate(mixed $value, string $expected, Context $context): bool
 	{
 		$isOk = $context->createChecker();
@@ -183,10 +183,8 @@ trait Base
 	}
 
 
-	/**
-	 * @deprecated use Nette\Schema\Helpers::validateRange()
-	 * @param  array{?float, ?float}  $range
-	 */
+	/** @param array{?float, ?float} $range */
+	#[\Deprecated('use Nette\Schema\Helpers::validateRange()')]
 	private static function doValidateRange(mixed $value, array $range, Context $context, string $types = ''): bool
 	{
 		$isOk = $context->createChecker();
@@ -195,7 +193,7 @@ trait Base
 	}
 
 
-	/** @deprecated use doTransform() */
+	#[\Deprecated('use doTransform()')]
 	private function doFinalize(mixed $value, Context $context): mixed
 	{
 		return $this->doTransform($value, $context);
